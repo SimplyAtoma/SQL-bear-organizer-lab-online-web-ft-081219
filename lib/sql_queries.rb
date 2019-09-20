2c159bacf81f2b5f4de7c96db5890928f0b68942
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-20 21:19:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT color FROM bears "
+  "SELECT COUNT(color) FROM bears "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT temperaments FROM bears WHERE temperaments = 'goofy'"
+  "SELECT COUNT(temperaments) FROM bears WHERE temperaments = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
